8e326c0ed25f5a83b376b8bbd24e24d99f28603f
JohnAguZ <Aguirrezabalaj69731@masseyhigh.school.nz>
2020-02-11 10:54:20 +1300

    Broken gui

--- 01_Help_GUI.py
+++ 01_Help_GUI.py
@@ -1,15 +1,26 @@
 from tkinter import *
+from functools import partial # To prevent unwanted windows
+
 import random
 
 
-class Foo:
-    def __init__(self, parent):
-        print("hello world")
+class Converter:
+    def __init__(self):
+
+        # Formatting Variables...
+        background_color = "light blue"
+
+        # Converter Main Screen GUI...
+        self.converter_frame = Frame(width=600, height=600, bg=background_color)
+        self.converter_frame.grid()
+
+        self.hello_label = Label(text="hello")
+        self.hello_label.grid(row=0)
+
 
 
 # main routine
 if __name__ == "__main__":
     root = Tk()
     root.title("Tempreture Converter")
-    something = Foo(root)
     root.mainloop()
